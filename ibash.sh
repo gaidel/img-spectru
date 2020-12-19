19cc12be6f30121dd66110090c2fe27ddd50d178
Ilya Petrov <ipetrov@byte-1c.ru>
2020-12-19 21:02:34 +0400

    Alias img-tool

--- ibash.sh
+++ ibash.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env bash
-docker run --privileged -i --rm -v $(pwd):/mnt urpylka/img-tool:0.7.1 /bin/bash img-tool $1 exec hzchevo.sh
+alias img-tool='docker run --privileged -i --rm -v $(pwd):/mnt urpylka/img-tool:0.7.1 /bin/bash img-tool'
+# $1 exec hzchevo.sh
